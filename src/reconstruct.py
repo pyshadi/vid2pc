0e72117a023e83fcda13037f78650cdd5721a82b
Shadi Alhaj <131242105+pyshadi@users.noreply.github.com>
2023-07-19 13:02:54 +0200

    Update reconstruct.py

--- src/reconstruct.py
+++ src/reconstruct.py
@@ -2,10 +2,9 @@ import numpy as np
 import open3d as o3d
 
 def merge_point_clouds(file_prefix, num_frames):
-    # Create an empty point cloud object
+    # Eempty point cloud object
     scene = o3d.geometry.PointCloud()
 
-    # Iterate over the frames and load each point cloud file
     for i in range(num_frames):
         filename = f"{file_prefix}{i}.pcd"
         cloud = o3d.io.read_point_cloud(filename)
@@ -15,14 +14,14 @@ def merge_point_clouds(file_prefix, num_frames):
 
     return scene
 
-# Specify the file prefix and the number of frames to process
+# File prefix and number of frames to process
 file_prefix = "../data/pc/frame_"
 num_frames = 10
 
 # Merge the point clouds into a scene
 scene = merge_point_clouds(file_prefix, num_frames)
 
-# Save the merged point cloud as a PCD file
+# Point cloud as a PCD file
 output_filename = "scene.pcd"
 o3d.io.write_point_cloud(output_filename, scene)
 
